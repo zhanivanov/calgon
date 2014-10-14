8771d5c1d7962013ff67d7d58432afc22a6e25e9
Zhan Ivanov <zhan.ivanovv@gmail.com>
2014-10-14 14:57:21 +0300

    - Utilities.cs added

---
+++ calgon/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calgon
+{
+    abstract class Utilities
+    {
+        public static void PrintStringOnPositon(int x, int y, string str, ConsoleColor color = ConsoleColor.Gray)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = color;
+            Console.Write(str);
+        }
+    }
+}
